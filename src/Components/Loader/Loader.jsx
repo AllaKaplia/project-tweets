8ec9bce71498ee1b1df234a3c42e27e4f3f6c77a
AllaKaplia <kaplia.alla18@gmail.com>
2023-10-14 01:31:27 +0300

    first commit

---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { Comment } from  'react-loader-spinner';
+
+const Loader = () => {
+    return (
+        <div>
+            <Comment
+            visible={true}
+            height="60"
+            width="60"
+            ariaLabel="comment-loading"
+            wrapperStyle={{}}
+            wrapperClass="comment-wrapper"
+            color="#fff"
+            backgroundColor="#471CA9"
+            />
+        </div>
+    )
+}
+
+export default Loader;

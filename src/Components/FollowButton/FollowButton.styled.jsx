d5253930e55439fe375805eb45494e6138916ef5
AllaKaplia <kaplia.alla18@gmail.com>
2023-11-04 22:35:38 +0200

    update button style

--- src/Components/FollowButton/FollowButton.styled.jsx
+++ src/Components/FollowButton/FollowButton.styled.jsx
@@ -1,18 +1,25 @@
 import styled from 'styled-components';
 
 export const ButtonFollow = styled.button`
-    background: #EBD8FF;
     position: relative;
+    margin-top: 390px;
+    z-index: 1000;
     margin: 0 auto;
     display: flex;
     width: 196px;
     padding: 14px 28px;
+
     justify-content: center;
     align-items: center;
     gap: 6px;
+
     border-radius: 10.311px;
     box-shadow: 0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.25);
     color: #373737;
+    background: #EBD8FF;
+    cursor: pointer;
+
+
     font-family: inherit;
     font-size: 18px;
     font-style: normal;
@@ -20,6 +27,4 @@ export const ButtonFollow = styled.button`
     line-height: normal;
     text-transform: uppercase;
     border: none;
-    margin-top: 390px;
-    z-index: 1000;
 `;
